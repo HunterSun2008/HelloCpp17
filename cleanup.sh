ff02a0079ec7f8080ca2cebdf0a260f9705b1481
HunterSun2008 <sht@palliums.org>
2019-04-09 16:20:19 +0800

    remove .vscode/ipch

--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,4 @@ rm -f config.guess config.h.in config.h.in~ config.sub configure compile
 rm -f install-sh ltmain.sh 
 rm -r -f build android win32 m4 depcomp missing
 rm -r -f src/Makefile.in test/Makefile.in
+rm -r -f .vscode/ipch
